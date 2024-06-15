07a495769321fe5f5345bbddfaa7355366e9db48
devdeliw <devaldeliwala@Devals-MacBook-Pro.local>
2024-06-15 12:50:56 -0700

    script to run red_clump_analysis across all filters

--- work/red_clump_script.py
+++ work/red_clump_script.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 from astropy.table import Table
+from red_clump_analysis import riemann_result
+import os
 
 zeropoints = {
     'Region': ['NRCB1', 'NRCB2', 'NRCB3', 'NRCB4', 'NRCB5'],
@@ -10,68 +12,145 @@ zeropoints = {
     'F405N': [0, 0, 0, 0, 20.9], 
 }
 
-spisea_filters = {
-    'Filter': ['F115W', 'F212N', 'F323N', 'F405N'], 
-    'SPISEA': ['jwst,F115W', 'jwst,F212N', 'jwst,F323N', 'jwst,F405N']
-}
+spisea_filters= pd.DataFrame(
+    ['jwst,F115W', 'jwst,F212N', 'jwst,F323N', 'jwst,F405N'],
+    index=['F115W', 'F212N', 'F323N', 'F405N'],
+    columns = ['SPISEA'])
+
+x_range_ = { 
+	'x1' : ['F115W', 'F212N', 'F323N', 'F405N'], 
+	'F115W' : [None, [5.8, 9], [6, 10], [7.5, 11.5]], 
+	'F212N' : [None, None, None, [1.3, 2.6]], 
+	'F323N' : [None, None, None, [0.4, 3]]}
 
 f115w_x1_f212n_x2 = { 
-    'F115W' : [(6.3, 21.3), (9, 25.2), (6.3, 22), (9, 25.9)],
-    'F212N' : [(6.2, 15.5), (7.9, 16.3), (6.65, 15.2), (8.35, 16.0)] 
-}
+    'F115W' : [[(6.3, 21.3), (9, 25.2)], [(6.3, 22), (9, 25.9)]],
+    'F212N' : [[(6.2, 15.5), (7.9, 16.3)], [(6.65, 15.2), (8.35, 16.0)]]}
 
 f115w_x1_f323n_x2 = {
-    'F115W' : [(6.9, 21.7), (8.5, 23.9), (7.5, 21.1), (9.1, 23.3)],
-    'F323N' : [(7.4, 13.75), (9.2, 14.8), (7.1, 14.8), (8.9, 15.85)]
-}
+    'F115W' : [[(6.9, 21.7), (8.5, 23.9)], [(7.5, 21.1), (9.1, 23.3)]],
+    'F323N' : [[(7.4, 13.75), (9.2, 14.8)], [(7.1, 14.8), (8.9, 15.85)]]}
 
 f115w_x1_f405n_x2 = { 
-    'F115W' : [(7.85, 21), (10, 23.5), (8, 21.95), (10.15, 24.45)],
-    'F405N' : [(7.9, 13.3), (10.5, 13.8), (8.4, 14.1), (11, 14.6)]
-}
+    'F115W' : [[(7.85, 21), (10, 23.5)], [(8, 21.95), (10.15, 24.45)]],
+    'F405N' : [[(7.9, 13.3), (10.5, 13.8)], [(8.4, 14.1), (11, 14.6)]]}
 
 f212n_x1_f323n_x2 = {
     'F212N' : 0,
-    'F323N' : 0
-}
+    'F323N' : 0}
 
 f212n_x1_f405n_x2 = {
-    'F212N' : [(1.6, 14.9), (2.15, 15.85), (1.55, 15.5), (2.1, 16.45)],
-    'F405N' : [(1.5, 13.15), (2.2, 13.55), (1.6, 14.1), (2.3, 14.5)]
-}
+    'F212N' : [[(1.6, 14.9), (2.15, 15.85)], [(1.55, 15.5), (2.1, 16.45)]],
+    'F405N' : [[(1.5, 13.15), (2.2, 13.55)], [(1.6, 14.1), (2.3, 14.5)]]}
 
 f323n_x1_f405n_x2 = { 
-    'F323N' : [(0.5, 13.7), (2, 15.75), (0.5, 14.8), (2, 16.85)],
-    'F405N' : [(0.5, 14.3), (2.6, 15), (0.7, 13), (2.8, 13.7)]
-}
+    'F323N' : [[(0.5, 13.7), (2, 15.75)], [(0.5, 14.8), (2, 16.85)]],
+    'F405N' : [[(0.5, 14.3), (2.6, 15)], [(0.7, 13), (2.8, 13.7)]]}
 
 f115w_x1 = { 
     'F212N' : f115w_x1_f212n_x2,
     'F323N' : f115w_x1_f323n_x2,
-    'F405N' : f115w_x1_f405n_x2
-}
+    'F405N' : f115w_x1_f405n_x2}
 
 f212n_x1 = { 
     'F323N' : f212n_x1_f323n_x2,
-    'F405N' : f212n_x1_f405n_x2
-}
+    'F405N' : f212n_x1_f405n_x2}
 
 f323n_x1 = { 
-    'F405N' : f323n_x1_f405n_x2
-}
+    'F405N' : f323n_x1_f405n_x2}
 
-x1 = pd.DataFrame({ 
+cutoffs = pd.DataFrame({ 
     'F115W' : f115w_x1,
     'F212N' : f212n_x1,
-    'F323N' : f323n_x1
-})
+    'F323N' : f323n_x1})
 
-zeropoints = pd.DataFrame(zeropoints)
-spisea_filters = pd.DataFrame(spisea_filters)
+zeropoints = pd.DataFrame(zeropoints).set_index('Region')
+x_range_ = pd.DataFrame(x_range_).set_index('x1')
 
 fits ='~/research/work/catalogs/dr2/jwst_init_NRCB.fits'
 catalog = Table.read(fits, format='fits')
 
+# Add what you want to run. [[[x1, x2, y]], ...] for an x1-x2 vs y CMD
+script = [[['NRCB1', 'F115W'], ['NRCB1', 'F212N'], ['NRCB1', 'F115W']],
+		  [['NRCB1', 'F115W'], ['NRCB1', 'F212N'], ['NRCB1', 'F212N']],
+		  [['NRCB2', 'F115W'], ['NRCB2', 'F212N'], ['NRCB2', 'F115W']],
+		  [['NRCB2', 'F115W'], ['NRCB2', 'F212N'], ['NRCB2', 'F212N']],
+		  [['NRCB3', 'F115W'], ['NRCB3', 'F212N'], ['NRCB3', 'F115W']],
+		  [['NRCB3', 'F115W'], ['NRCB3', 'F212N'], ['NRCB3', 'F212N']],
+		  [['NRCB4', 'F115W'], ['NRCB4', 'F212N'], ['NRCB4', 'F115W']],
+		  [['NRCB4', 'F115W'], ['NRCB4', 'F212N'], ['NRCB4', 'F212N']],
+		  [['NRCB1', 'F115W'], ['NRCB5', 'F323N'], ['NRCB1', 'F115W']],
+		  [['NRCB1', 'F115W'], ['NRCB5', 'F323N'], ['NRCB5', 'F323N']],
+		  [['NRCB2', 'F115W'], ['NRCB5', 'F323N'], ['NRCB2', 'F115W']],
+		  [['NRCB2', 'F115W'], ['NRCB5', 'F323N'], ['NRCB5', 'F323N']],
+		  [['NRCB3', 'F115W'], ['NRCB5', 'F323N'], ['NRCB3', 'F115W']],
+		  [['NRCB3', 'F115W'], ['NRCB5', 'F323N'], ['NRCB5', 'F323N']],
+		  [['NRCB4', 'F115W'], ['NRCB5', 'F323N'], ['NRCB4', 'F115W']],
+		  [['NRCB4', 'F115W'], ['NRCB5', 'F323N'], ['NRCB5', 'F323N']],
+		  [['NRCB1', 'F115W'], ['NRCB5', 'F405N'], ['NRCB1', 'F115W']],
+		  [['NRCB1', 'F115W'], ['NRCB5', 'F405N'], ['NRCB5', 'F405N']],
+		  [['NRCB2', 'F115W'], ['NRCB5', 'F405N'], ['NRCB2', 'F115W']],
+		  [['NRCB2', 'F115W'], ['NRCB5', 'F405N'], ['NRCB5', 'F405N']],
+		  [['NRCB3', 'F115W'], ['NRCB5', 'F405N'], ['NRCB3', 'F115W']],
+		  [['NRCB3', 'F115W'], ['NRCB5', 'F405N'], ['NRCB5', 'F405N']],
+		  [['NRCB4', 'F115W'], ['NRCB5', 'F405N'], ['NRCB4', 'F115W']],
+		  [['NRCB4', 'F115W'], ['NRCB5', 'F405N'], ['NRCB5', 'F405N']],
+		  [['NRCB1', 'F212N'], ['NRCB5', 'F405N'], ['NRCB1', 'F212N']],
+		  [['NRCB1', 'F212N'], ['NRCB5', 'F405N'], ['NRCB5', 'F405N']],
+		  [['NRCB2', 'F212N'], ['NRCB5', 'F405N'], ['NRCB2', 'F212N']],
+		  [['NRCB2', 'F212N'], ['NRCB5', 'F405N'], ['NRCB5', 'F405N']],
+		  [['NRCB3', 'F212N'], ['NRCB5', 'F405N'], ['NRCB3', 'F212N']],
+		  [['NRCB3', 'F212N'], ['NRCB5', 'F405N'], ['NRCB5', 'F405N']],
+		  [['NRCB4', 'F212N'], ['NRCB5', 'F405N'], ['NRCB4', 'F212N']],
+		  [['NRCB4', 'F212N'], ['NRCB5', 'F405N'], ['NRCB5', 'F405N']],
+]
+
+
+# ------------------------------------------------------------------- #
+
+for i in range(len(script)): 
+
+	catalog1filt = script[i][0][1]
+	catalog2filt = script[i][1][1]
+	catalogyfilt = script[i][2][1]
+
+	region1 = script[i][0][0]
+	region2 = script[i][1][0]
+	regiony = script[i][2][0]
+
+	filters = [spisea_filters['SPISEA'][catalog1filt], spisea_filters['SPISEA'][catalog2filt]]
+	parallel_cutoff1 = cutoffs[catalog1filt][catalog2filt][catalogyfilt][0]
+	parallel_cutoff2 = cutoffs[catalog1filt][catalog2filt][catalogyfilt][1]
+	x_range = x_range_[catalog1filt][catalog2filt]
+	catalog1zp = zeropoints[catalog1filt][region1]
+	catalog2zp = zeropoints[catalog2filt][region2]
+
+	n = 10
+	image_path = f"/Users/devaldeliwala/research/work/plots&data/rc_analysis_plots/{region1}/{catalog1filt}-{catalog2filt}/vs{catalogyfilt}/"
+	hists = [True, False]
+
+	if not os.path.isdir(image_path):
+		os.makedirs(image_path)
+
+	print("")
+	print(f"Current: {region1} {catalog1filt} - {region2} {catalog2filt} vs. {regiony} {catalogyfilt}")
+	print(f"##################################################")
+	print("")
+
+	riemann_result(
+		catalog, catalog1filt, catalog2filt, catalogyfilt, 
+		region1, region2, regiony, filters, 
+		parallel_cutoff1, parallel_cutoff2, 
+		x_range, n, image_path, hists[0], 
+		catalog1zp, catalog2zp, 
+	)
+	riemann_result(
+		catalog, catalog1filt, catalog2filt, catalogyfilt, 
+		region1, region2, regiony, filters, 
+		parallel_cutoff1, parallel_cutoff2, 
+		x_range, n, image_path, hists[1], 
+		catalog1zp, catalog2zp, 
+	)
 
 
 
