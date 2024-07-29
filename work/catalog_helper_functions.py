81370da835dcb6996018bcad6cab8ee7a27af385
devdeliw <devaldeliwala@Devals-MacBook-Pro.local>
2024-07-29 09:34:45 -0700

    python methods to help parse through starlists

---
+++ work/catalog_helper_functions.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+def get_all_filts(t):
+    """
+    Helper function to get filts in a catalog, regardless of
+    masking
+    """
+    nepochs = len(t['filt'][0])
+
+    # Loop through each time entry and get year
+    # from a non-masked source
+    filt_arr = []
+    for ii in range(nepochs):
+        filt_col = t['filt'][:,ii]
+
+        good = np.where(t['x'][:,ii].mask == False)[0]
+        if len(good) > 0:
+            filt_arr.append(t['filt'][good[0],ii])
+        else:
+            filt_arr.append(np.nan)
+       
+
+    filt_arr = np.array(filt_arr)
+   
+    return filt_arr
+
+def get_all_dets(t): 
+    """
+    Helper function to get detectors in a catalog, regardless of
+    masking
+    """
+    nepochs = len(t['det'][0])
+
+    # Loop through each time entry and get year
+    # from a non-masked source
+    det_arr = []
+    for ii in range(nepochs):
+        det_col = t['det'][:,ii]
+
+        good = np.where(t['x'][:,ii].mask == False)[0]
+        if len(good) > 0:
+            det_arr.append(t['det'][good[0],ii])
+        else:
+            det_arr.append(np.nan)
+
+    det_arr = np.array(det_arr)
+   
+    return det_arr
+
+def get_matches(t, filt1, det1, filt2, det2): 
+    filt = get_all_filts(t)
+    det = get_all_dets(t)
+
+    idx1 = np.where( (filt == filt1) & (det == det1) ) 
+    idx2 = np.where( (filt == filt2) & (det == det2) ) 
+
+    filt_1 = t['m_orig'][:,idx1]
+    filt_2 = t['m_orig'][:,idx2]
+
+    filt_1me = t['me_orig'][:,idx1]
+    filt_2me = t['me_orig'][:,idx2]
+    
+    good = np.where( (filt_1.mask == False) & (filt_2.mask == False) )
+    m_filt1_match = filt_1[good]
+    m_filt2_match = filt_2[good]
+    me_filt1_match = filt_1me[good]
+    me_filt2_match = filt_2me[good]
+
+    return m_filt1_match, m_filt2_match, me_filt1_match, me_filt2_match
+
